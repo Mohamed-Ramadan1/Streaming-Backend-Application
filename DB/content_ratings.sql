226a5aed2525100b5047ea648805c8fb0ed36693
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2024-10-12 18:35:07 +0300

    enhance project setup.

--- DB/content_ratings.sql
+++ DB/content_ratings.sql
@@ -1,7 +1,7 @@
-id => serial
-user_id      =>
-content_id =>
-content_type =>
+id => serial PRIMERY KEY UNIQUE AUTO INCREMENT
+user_id => BIGINT NOT NULL FORIGN KEY FROM USERS TABLE
+content_id => BIGINT NOT NULL FORIGN KEY FROM CONTENT TABLE
+content_type => 
 rating_value =>
 created_at =>
 updated_at =>
